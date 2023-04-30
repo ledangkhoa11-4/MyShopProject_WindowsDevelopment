b167743d7af23cc0a8c836743ca2dad4f56ab1e7
phuoc <nguyennhuphuoc2002@gmail.com>
2023-04-30 19:08:06 +0700

    fix account error

--- DAO/Account_DAO.cs
+++ DAO/Account_DAO.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,11 +15,31 @@ namespace MyShopProject.DAO
     class Account_DAO
     {
        
-        public async Task<Account> getAccount(string content)
+        public async Task<Account> getAccount(string username,string password,string salt)
         {
-            var json = await API.getMethod($"/account?username={content}");
+            var json = await API.getMethod($"/account?username={username}");
             var account = JsonConvert.DeserializeObject<Account>(json);
-            return account;
+            if(account != null)
+            {
+                byte[] entropyInBytes1 = Convert.FromBase64String(salt);
+                byte[] cypherTextInBytes1 = Convert.FromBase64String(password);
+                byte[] passwordInBytes1 = ProtectedData.Unprotect(cypherTextInBytes1,
+                    entropyInBytes1,
+                    DataProtectionScope.CurrentUser
+                );
+                string pass1 = Encoding.UTF8.GetString(passwordInBytes1);
+
+                byte[] entropyInBytes2 = Convert.FromBase64String(account.Salt);
+                byte[] cypherTextInBytes2 = Convert.FromBase64String(account.Password);
+                byte[] passwordInBytes2 = ProtectedData.Unprotect(cypherTextInBytes2,
+                    entropyInBytes2,
+                    DataProtectionScope.CurrentUser
+                );
+                string pass2 = Encoding.UTF8.GetString(passwordInBytes2);
+                if (pass1 == pass2)
+                    return account;
+            }
+            return null;
         }
 
         public async Task<bool> checkExist(string content)
